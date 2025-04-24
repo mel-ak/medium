6e13adc2ef107c7043d679e2a6374cc81082ab13
mel-ak <melakesisay@gmail.com>
2025-04-24 15:07:43 +0300

    Medium Clone 1st Day

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+
+    <div class="py-4">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-4 text-gray-900">
+                    <x-category-tabs />
+                </div>
+            </div>
+
+            <div class="mt-8 text-gray-900">
+                
+                @forelse ($posts as $post)
+                    <x-post-item :post="$post" />
+                @empty
+
+                    <div class="text-gray-400 text-center p-16">No posts found</div>
+
+                @endforelse
+            </div>
+
+            {{ $posts->onEachSide(1)->links() }}
+        </div>
+    </div>
+</x-app-layout>
